3e9fac1fcd2a20439bfb2e3fc940b5f2a470dc0b
大崔哥 <18640298870@163.com>
2020-06-04 16:23:53 +0800

    建立服务注册与发现服务器

---
+++ src/test/java/com/example/daveurakaserver/DavEurakaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daveurakaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavEurakaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
